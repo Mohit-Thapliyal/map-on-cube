fec857a033f089b40ddda96b6cf3aafec1c9148d
Mohit Thapliyal <mohitthapliyal100@gmail.com>
2023-08-15 21:19:52 +0530

    updated key

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,22 +1,10 @@
-import { useMemo, useState, forwardRef } from "react";
+import { useMemo, forwardRef } from "react";
 import { GoogleMap, useLoadScript, MarkerF } from "@react-google-maps/api";
-// import usePlacesAutocomplete, {
-//   getGeocode,
-//   getLatLng,
-// } from "use-places-autocomplete";
-// import {
-//   Combobox,
-//   ComboboxInput,
-//   ComboboxPopover,
-//   ComboboxList,
-//   ComboboxOption,
-// } from "@reach/combobox";
 
 const libraries = ["places"];
 
 const Map = forwardRef((props, ref) => {
   const { isLoaded } = useLoadScript({
-    googleMapsApiKey: "AIzaSyCew9_bIsxbq6eNSD4mb2qJqMGdjUOVZFQ",
     libraries: libraries,
   });
 
@@ -26,16 +14,12 @@ const Map = forwardRef((props, ref) => {
 
 const MapFrame = forwardRef((props, ref) => {
   const center = useMemo(() => ({ lat: 28.7041, lng: 77.1025 }), []);
-  //   const [selected, setSelected] = useState(null);
   const defaultMapOptions = {
     fullscreenControl: false,
     disableDefaultUI: true,
   };
   return (
     <div className="h-full w-full relative" ref={ref}>
-      {/* <div className="absolte top-2 left-2 rounded-sm overflow-hiden z-10">
-        <PlacesAutocomplete setSelected={setSelected} />
-      </div> */}
       <GoogleMap
         ref={ref}
         zoom={11}
@@ -48,34 +32,5 @@ const MapFrame = forwardRef((props, ref) => {
     </div>
   );
 });
-
-// const PlacesAutocomplete = ({ setSelected }) => {
-//   const {
-//     ready,
-//     value,
-//     setValue,
-//     suggestions: { status, data },
-//     clearSuggestions,
-//   } = usePlacesAutocomplete();
-//   return (
-//     <Combobox>
-//       <ComboboxInput
-//         className="p-1"
-//         value={value}
-//         onChange={(e) => setValue(e.target.value)}
-//         disabled={!ready}
-//         placeholder="Search an address"
-//       />
-//       <ComboboxPopover>
-//         <ComboboxList>
-//           {status === "OK" &&
-//             data.map(({ place_id, description }) => (
-//               <ComboboxOption key={place_id} value={description} />
-//             ))}
-//         </ComboboxList>
-//       </ComboboxPopover>
-//     </Combobox>
-//   );
-// };
 
 export default Map;
